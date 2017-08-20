2e7c66fc378df26c2d42a3d30b745dcf32a4bcd9
mikeliu8492 <mikeliu2@illinois.edu>
2017-08-20 12:46:34 -0600

    changed up header display

--- scripts/react_header_render.js
+++ scripts/react_header_render.js
@@ -1,55 +1,55 @@
 var rootElementHead =
     React.createElement('ul', {},
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: '/'
             }, 'Home')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: 'mailto:mikeliu2@illinois.edu'
             }, 'Email')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: 'resume.pdf'
             }, 'Resume')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: 'https://github.com/mikeliu8492'
             }, 'Github')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: 'https://www.linkedin.com/in/mikeliu8492'
             }, 'LinkedIn')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: '/#work'
             }, 'Work Experience')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: '/projects'
             }, 'Projects')
         ),
-        React.createElement('li', {},
+        React.createElement('li', { class: "headerlist" },
             React.createElement('a', {
                 href: '/musicalsurprise'
             }, 'A Very Nice (Musical?) Surprise')
         )
     )
-    
-    var rootElementFoot = React.createElement('ul', {},
-            React.createElement('li', {},
-                React.createElement('a', {
-                    href: '#PageTitle'
-                }, 'Back to Top')
-            )
-        )
-     
+
+var rootElementFoot = React.createElement('ul', {},
+    React.createElement('li', {},
+        React.createElement('a', {
+            href: '#PageTitle'
+        }, 'Back to Top')
+    )
+)
+
 
 ReactDOM.render(rootElementHead, document.getElementById('introheader'))
 ReactDOM.render(rootElementFoot, document.getElementById('myfooter'))
